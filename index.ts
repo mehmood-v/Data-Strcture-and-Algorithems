ac4bb0bc908ff712f420513a72d45b776fc6fa52
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-02-16 15:09:38 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -11,12 +11,15 @@ const maxSum = (arr: number[], k = 4) => {
   let i = 0;
   let j = arr.length - 1;
   let tempSum = 0;
-  let totalSum = 0;
+  let maxSum = 0;
   for (let i = 0; i <= k; i++) {
     tempSum += arr[i];
   }
 
-  console.log(tempSum);
+  for (let i = k; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - k] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
 };
 
 maxSum([1, 4, 6, 4, 7, 9, 5, 6, 4]);
