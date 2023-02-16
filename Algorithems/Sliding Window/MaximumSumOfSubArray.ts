d63d54799930cb731c98cf524a4e38fae3c8034e
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-02-16 14:42:48 +0100

    2 files modified

--- Algorithems/Sliding Window/MaximumSumOfSubArray.ts
+++ Algorithems/Sliding Window/MaximumSumOfSubArray.ts
@@ -1,6 +1,14 @@
-export default (arr: number[], k = 4) => {
+const maxSum = (arr: number[], k = 4) => {
   let i = 0;
   let j = arr.length - 1;
+  let tempSum = 0;
+  let totalSum = 0;
+  for (let i = 0; i <= k; i++) {
+    tempSum += arr[i];
+  }
 
-  while (i < j) {}
+  console.log(tempSum);
 };
+
+maxSum([1, 4, 6, 4, 7, 9, 5, 6, 4]);
+export default maxSum;
--- index.ts
+++ index.ts
@@ -6,3 +6,18 @@
 // charCount('The Quick brown fox jumps over the lazy dog');
 
 //console.log(AnagramSolution('anagram', 'nagaram'));
+
+const maxSum = (arr: number[], k = 4) => {
+  let i = 0;
+  let j = arr.length - 1;
+  let tempSum = 0;
+  let totalSum = 0;
+  for (let i = 0; i <= k; i++) {
+    tempSum += arr[i];
+  }
+
+  console.log(tempSum);
+};
+
+maxSum([1, 4, 6, 4, 7, 9, 5, 6, 4]);
+export default maxSum;
