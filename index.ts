007d8f3f89b206aa8df40cad32d99f77338810d1
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-02-17 11:24:44 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -6,21 +6,3 @@
 // charCount('The Quick brown fox jumps over the lazy dog');
 
 //console.log(AnagramSolution('anagram', 'nagaram'));
-
-// function maxSubarraySum(arr: number[], num: number) {
-//   let maxSum = 0;
-//   let tempSum = 0;
-//   if (arr.length < num) return null;
-//   for (let i = 0; i < num; i++) {
-//     maxSum += arr[i];
-//   }
-//   tempSum = maxSum;
-//   for (let i = num; i < arr.length; i++) {
-//     tempSum = tempSum - arr[i - num] + arr[i];
-//     maxSum = Math.max(maxSum, tempSum);
-//   }
-//   return maxSum;
-// }
-
-// maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
-// export default maxSubarraySum;
