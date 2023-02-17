b4162f5692d324e459bc7040aa9474791be8f67e
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-02-17 12:24:37 +0100

    1 file modified

--- index.ts
+++
@@ -1,8 +0,0 @@
-// import { charCount } from './Problems/CharacterCount/FindCharactersCount';
-//import AnagramSolution from './Problems/Anagram Problem';
-// import FrequencyCounterBetterSolution from './Problems/FrequencyCounterProblem/BetterSolution';
-
-// console.log(FrequencyCounterBetterSolution);
-// charCount('The Quick brown fox jumps over the lazy dog');
-
-//console.log(AnagramSolution('anagram', 'nagaram'));
