51f7669b16fe44fb99f743f40519ea402009125c
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-02-18 07:42:40 +0100

    1 file modified

---
+++ index.ts
@@ -0,0 +1,30 @@
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+// Examples:
+
+//Input  areThereDuplicates(1, 2, 3)
+
+// Output: false
+
+// areThereDuplicates(1, 2, 3)
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+
+export default function areThereDuplicates(...args: string[] | number[]) {
+  let left = 0;
+  let right = args.length - 1;
+  let result = true;
+
+  while (left < right) {
+    if (args[left] == args[right]) {
+      result = false;
+    } else {
+      ++left;
+      --right;
+    }
+  }
+
+  console.log(result);
+}
+
+areThereDuplicates(1, 2, 3);
