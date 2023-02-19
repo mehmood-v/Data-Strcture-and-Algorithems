0217204245faad3dcc710d841e6fbb1018940984
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-02-19 09:58:26 +0100

    2 files modified

--- Algorithems/Sliding Window/MaximumSumOfSubArray.ts
+++ Algorithems/Sliding Window/MaximumSumOfSubArray.ts
@@ -1,12 +1,18 @@
-// Find the maximum sum of a Sub Array
+// Sliding Window - maxSubarraySum
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
 
-// Input:
+// Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
 
-//Array: [1, 4, 6, 4, 7, 9, 5, 6, 4]
+// maxSubarraySum([100,200,300,400], 2) // 700
+// maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)  // 39
+// maxSubarraySum([-3,4,0,-2,6,-1], 2) // 5
+// maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2) // 5
+// maxSubarraySum([2,3], 3) // null
+// Constraints:
 
-// Number: 4
+// Time Complexity - O(N)
 
-//  Output: Maximum count of sub array of size 4
+// Space Complexity - O(1)
 
 const maxSum = (array: number[], num = 4) => {
   if (array.length < num) return;
