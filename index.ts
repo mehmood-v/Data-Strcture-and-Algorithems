5dd51d1b78c6001fdcd4a25ee258ce104dab1046
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-03-11 02:11:45 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -3,13 +3,10 @@
 // recursiveRange(10) // 55
 
 function recursiveRange(num) {
-  if (num <= 1) {
-    return 1;
+  if (num === 0) {
+    return 0;
   }
-  console.log(num);
-  // return num + recursiveRange(num - 1);
+  return num + recursiveRange(num - 1);
 }
 
 console.log(recursiveRange(6));
-
-console.log('JIII');
