72c6f8aca610b24afc1aeb5111a1a3b6b45a2c88
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-03-12 02:51:03 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -4,9 +4,22 @@ function binarySearch(array, num) {
   let start = 0;
   let end = array.length - 1;
 
-  let mid = start + (end - start) / 2;
+  let mid = Math.floor(start + (end - start) / 2);
 
-  console.log(mid);
+  while (mid !== num && start <= end) {
+    if (num > mid) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+
+    mid = Math.floor(start + (end - start) / 2);
+  }
+
+  if (array[mid] == num) {
+    return mid;
+  }
+  return -1;
 }
 
 binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
