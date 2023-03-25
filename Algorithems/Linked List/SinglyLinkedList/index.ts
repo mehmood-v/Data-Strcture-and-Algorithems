7672224570517f9c77c15ace07e1a2ba40b7b5ab
mehmood-v <95611144+mehmood-v@users.noreply.github.com>
2023-03-25 03:19:38 +0100

    1 file modified

--- Algorithems/Linked List/SinglyLinkedList/index.ts
+++ Algorithems/Linked List/SinglyLinkedList/index.ts
@@ -51,4 +51,17 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    this.head = current.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return current;
+  }
 }
